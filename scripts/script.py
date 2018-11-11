49731c71fd547bbd4749d3019fe89da34031e2d9
ivarprudnikov <ivar.prudnikov@gmail.com>
2018-11-11 08:09:24 +0000

    add simple Python script

---
+++ scripts/script.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys, getopt, time
+
+def main(argv):
+    argument = ''
+    usage = 'usage: script.py -f <sometext>'
+    try:
+        opts, args = getopt.getopt(argv,"hf:",["foo="])
+    except getopt.GetoptError:
+        print(usage)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(usage)
+            sys.exit()
+        elif opt in ("-f", "--foo"):
+            argument = arg
+
+    print "Start : %s" % time.ctime()
+    time.sleep( 5 )
+    print('Foo is')
+    time.sleep( 5 )
+    print(argument)
+    print "End : %s" % time.ctime()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
